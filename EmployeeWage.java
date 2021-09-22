ccc31ac8f9251ef5d2be74df69e2357249251db9
prajakta <prajaktamahajan789@gmail.com>
2021-09-22 16:54:37 +0530

    Add part time employee wages

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,27 +3,32 @@ package com.bridgelabz.Employee_wage;
 import java.util.Random;
 
 public class EmployeeWage {
-
-	public static void main(String[] args) {
-		int IS_FULL_TIME = 1;
-		int EMP_RATE_PER_HOUR = 20;
-		int empHours = 0;
-		int empWage = 0;
-		// Taking Random values 0 or 1.
-		Random random = new Random();
-		int empCheck = random.nextInt(2);
-		// Checking the condition.
-		if (empCheck == IS_FULL_TIME){
-			empHours = 8;
-		}
-		else {
-			empHours = 0;
-		}
-		empWage = empHours * EMP_RATE_PER_HOUR;
-		System.out.println("The daily wages of an employee is : " + empWage);
-
-	}
+	
+    public static void main(String[] args) {
+        int IS_FULL_TIME = 2;
+        int IS_PART_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHours = 0;
+        int empWage = 0;
+        
+        Random random = new Random();
+        int empCheck = random.nextInt(3);
+        
+        if (empCheck == IS_FULL_TIME){
+            empHours = 8;
+        }
+        else if (empCheck == IS_PART_TIME){
+            empHours = 4;
+        }
+        else {
+            empHours = 0;
+        }
+        empWage = empHours * EMP_RATE_PER_HOUR;
+        System.out.println("The daily wages of an employee is : " + empWage);
+    }
 }
+
+
 
 
 
