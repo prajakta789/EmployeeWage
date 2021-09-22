2dce08b1950d0e51ece89989cac6e30878b7fea8
prajakta <prajaktamahajan789@gmail.com>
2021-09-22 17:17:48 +0530

    Solving using switch case statement

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,29 +3,30 @@ package com.bridgelabz.Employee_wage;
 import java.util.Random;
 
 public class EmployeeWage {
-	
-    public static void main(String[] args) {
-        int IS_FULL_TIME = 2;
-        int IS_PART_TIME = 1;
-        int EMP_RATE_PER_HOUR = 20;
-        int empHours = 0;
-        int empWage = 0;
-        
-        Random random = new Random();
-        int empCheck = random.nextInt(3);
-        
-        if (empCheck == IS_FULL_TIME){
-            empHours = 8;
-        }
-        else if (empCheck == IS_PART_TIME){
-            empHours = 4;
-        }
-        else {
-            empHours = 0;
-        }
-        empWage = empHours * EMP_RATE_PER_HOUR;
-        System.out.println("The daily wages of an employee is : " + empWage);
-    }
+
+        public static final int IS_FULL_TIME = 2;
+        public static final int IS_PART_TIME = 1;
+        public static final int EMP_RATE_PER_HOUR = 20;
+
+	public static void main(String[] args) {
+		int empHours = 0;
+		int empWage = 0;
+		
+		Random random = new Random();
+		int empCheck = random.nextInt(3);
+		switch (empCheck){
+		case IS_FULL_TIME:
+			empHours = 8;
+			break;
+		case IS_PART_TIME:
+			empHours = 4;
+			break;
+		default:
+			empHours = 0;
+		}
+		empWage = empHours * EMP_RATE_PER_HOUR;
+		System.out.println("The daily wages of an employee is : " + empWage);
+	}
 }
 
 
