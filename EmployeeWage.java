b244d8203fd8aab6fc8f68737c4667b9b48e37c8
prajakta <prajaktamahajan789@gmail.com>
2021-09-22 16:43:45 +0530

    Calculate daily employee wages

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,16 +1,30 @@
 package com.bridgelabz.Employee_wage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHours = 0;
+		int empWage = 0;
+		// Taking Random values 0 or 1.
+		Random random = new Random();
+		int empCheck = random.nextInt(2);
+		// Checking the condition.
+		if (empCheck == IS_FULL_TIME){
+			empHours = 8;
+		}
+		else {
+			empHours = 0;
+		}
+		empWage = empHours * EMP_RATE_PER_HOUR;
+		System.out.println("The daily wages of an employee is : " + empWage);
 
-		double empcheck = Math.floor(Math.random() * 10) % 2;
-		if  (empcheck == IS_FULL_TIME)
-
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
 	}
 }
+
+
+
 
