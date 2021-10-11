b503b15d96e5893a726c70a0e0dd6a725f53cea6
prajakta <prajaktamahajan789@gmail.com>
2021-10-11 21:26:00 +0530

    Manage employee wage of multiple companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,74 +1,74 @@
 package com.bridgelabz.Employee_wage;
 
 import java.util.Random;
-
-public class EmployeeWage {
+ public class EmployeeWage {
 
 
 	public static final int IS_FULLTIME = 1;
-	public static final int IS_PARTTIME = 2;
+    public static final int IS_PARTTIME = 2;
+    private int numOfCompany = 0;
+	private static CompanyInfo companyInfo;
+    private CompanyInfo[] companyInfoArray;
 
-	private final String companyName;
-	private final int empRatePerHour;
-	private final int noOfWorkingDays;
-	private final int maxHoursPerMonth;
-	private int totalEmpWage;
+    public EmployeeWage() {
+        companyInfoArray = new CompanyInfo[6];
+    }
 
+    private void addCompanyInfo(String companyName, int empRatePerHour, int noOfWorkingDays, int maxHoursPerMonth) {
+        companyInfoArray[numOfCompany] = new CompanyInfo(companyName, empRatePerHour, noOfWorkingDays, maxHoursPerMonth);
+        numOfCompany++;
+    }
 
+    private void computeEmpWage() {
+        for (int i = 0; i < numOfCompany; i++) {
+            companyInfoArray[i].setTotalEmpWage(this.computeWage(companyInfoArray[i]));
+            System.out.println(companyInfoArray[i]);
+        }
+    }
 
-	public EmployeeWage(String companyName, int empRatePerHour, int noOfWorkingDays, int maxHoursPerMonth) {
-		this.companyName = companyName;
-		this.empRatePerHour = empRatePerHour;
-		this.noOfWorkingDays = noOfWorkingDays;
-		this.maxHoursPerMonth = maxHoursPerMonth;
-	}
+    /*
+       We have used static method here so that we can directly call it inside main
+     */
+    public static int computeWage(CompanyInfo companyInfo) {
+        EmployeeWage.companyInfo = companyInfo;
+		// Declaring the variables
+        int empHrs = 0;
+        int empWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        int totalWage = 0;
+        /*
+         * Using random method to generate random numbers 0, 1 and 2
+         */
+        while (totalEmpHrs <= companyInfo.getMaxHoursPerMonth() && totalWorkingDays < companyInfo.getNoOfWorkingDays()) {
+            totalWorkingDays++;
+            Random random = new Random();
+            int empCheck = random.nextInt(3);
+            switch (empCheck) {
+                case IS_FULLTIME: // FullTime Employee
+                    empHrs = 8;
+                    break;
+                case IS_PARTTIME: // PartTime Employee
+                    empHrs = 4;
+                    break;
+                default:  // Employee is absent
+                    empHrs = 0;
+            }
+            empWage = companyInfo.getEmpRatePerHour() * empHrs;
+            totalEmpHrs += empHrs;
+        }
+         totalWage = totalEmpHrs * companyInfo.getEmpRatePerHour();
+         return totalWage;
+    }
 
-
-
-	public void computeWage() {
-
-		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpHrs = 0;
-		int totalWorkingDays = 0;
-
-		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < noOfWorkingDays) {
-			totalWorkingDays++;
-			Random random = new Random();
-			int empCheck = random.nextInt(3);
-			switch (empCheck) {
-			case IS_FULLTIME: 
-				empHrs = 8;
-				break;
-			case IS_PARTTIME: 
-				empHrs = 4;
-				break;
-			default:  
-				empHrs = 0;
-			}
-			empWage = empRatePerHour * empHrs;
-			totalEmpHrs += empHrs;
-			System.out.println("Day " + totalWorkingDays + " : Employee worked : " + empHrs + " Hours " +
-					", Employee Wage : " + empWage );
-		}
-		totalEmpWage = totalEmpHrs * empRatePerHour;
-	}
-
-	@Override
-	public String toString(){
-		return "Total Wages of an employee in " +companyName + " company is : " + totalEmpWage + "\n";
-	}
-
-	public static void main(String[] args) {
-
-		EmployeeWage infosys = new EmployeeWage("Infosys", 200, 2, 10);
-		EmployeeWage tcs = new EmployeeWage("TCS" , 300, 3, 10);
-		EmployeeWage capgemini = new EmployeeWage("Capgemini", 155, 4, 12);
-		infosys.computeWage();
-		System.out.println(infosys);
-		tcs.computeWage();
-		System.out.println(tcs);
-		capgemini.computeWage();
-		System.out.println(capgemini);
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Program");
+        EmployeeWage employeeWage = new EmployeeWage();
+        employeeWage.addCompanyInfo("Infosys", 150, 2, 10);
+        employeeWage.addCompanyInfo("TCS" , 200, 3, 10);
+        employeeWage.addCompanyInfo("Capgemini", 100, 4, 12);
+        employeeWage.addCompanyInfo("Jio", 180, 3, 15);
+        employeeWage.addCompanyInfo("Airtel", 160, 4, 14);
+        employeeWage.computeEmpWage();
+    }
 }
